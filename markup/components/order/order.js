16efec130c33f9abf294658c630ab976eeb849e5
Catrina <skaya.online@gmail.com>
2018-12-02 22:29:02 +0200

    add success modal

--- markup/components/order/order.js
+++ markup/components/order/order.js
@@ -1,1 +1,13 @@
+(function () {
+  const form = document.querySelector('#order');
 
+  if (!form) return;
+
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    // if form success
+    const success = document.querySelector('.js-order-success');
+    success.click();
+  })
+})();
